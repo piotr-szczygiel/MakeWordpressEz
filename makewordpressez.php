150bbe316bf7ad7f70a2b89854cca63f77678055
Bogdan Juszczak <bogdan.juszczak@makingwaves.pl>
2014-10-24 13:52:28 +0200

    stuff

---
+++ makewordpressez.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: MakeWordpressEz
+ * Plugin URI:
+ * Description: shieeeeeet
+ * Version: 0.1
+ * Author: Bogdan
+ * Author URI: h
+ * Requires at least: 4.0
+ * Tested up to: 4.0
+ *
+ * Text Domain:
+ * Domain Path: /i18n/languages/
+ *
+ * @package MakeWordpressEz
+ * @category Core
+ * @author Bogdan
+ */
